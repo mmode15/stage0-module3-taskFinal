ec86f837805222d70208dacd35c80a8af9be019c
Maiko <maiko.mode05@gmail.com>
2023-09-12 12:35:00 +0400

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+
+    public  void revert(int number) {
+        int revertNumber = 0;
+        while(number > 0){
+            revertNumber += (number % 10) ;
+            number = (number -number % 10) /10;
+            revertNumber*=10;
+      }
+        System.out.println(revertNumber/10);
     }
 }
